e01fcd9ae651167b494cda4d1ed434f6bfeb9d24
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-03-12 15:37:04 +0530

    image upload fucntion

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+  }),
+);
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 const imageRouter = require("./router/image.router");
 
