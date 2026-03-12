009831599c9e9d41da96cfa9865b34167de504fd
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-03-12 16:35:56 +0530

    again the same

--- src/controller/image.controller.js
+++ src/controller/image.controller.js
@@ -1,32 +1,37 @@
 const Image = require("../model/image.model");
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
 
 exports.uploadImage = async (req, res) => {
   try {
-
-    console.log("FILE RECEIVED:", req.file);
-    console.log("BODY RECEIVED:", req.body);
-
     if (!req.file) {
-      return res.status(400).json({
-        message: "No file uploaded"
-      });
+      return res.status(400).json({ message: "No file uploaded" });
     }
 
-    const image = await Image.create({
-      image: req.file.path,
-      caption: req.body.caption
+    // Upload buffer to ImageKit
+    const result = await imagekit.upload({
+      file: req.file.buffer,           // file is in memory now
+      fileName: req.file.originalname,
     });
 
-    res.json({
-      message: "Image uploaded",
-      data: image
+    const image = await Image.create({
+      image: result.url,               // store ImageKit URL in MongoDB
+      caption: req.body.caption,
     });
+
+    res.json({ message: "Image uploaded", data: image });
 
   } catch (error) {
     console.error("UPLOAD ERROR:", error);
     res.status(500).json({ error: error.message });
   }
 };
+
 
 exports.getImages = async (req, res) => {
   const images = await Image.find();
