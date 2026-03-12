6c28a4c9a3aedb5f5de290e9bfe9262d0af96439
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-03-12 14:17:25 +0530

    port change

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 const app =  require("./src/app")
 const connectDB = require("./src/config/db")
+const PORT = process.env.PORT || 3000
 
 connectDB()
 
-app.listen(3000 , ()=>{
+app.listen(PORT , ()=>{
    console.log("server connected successfully")
 })
