096b66fb79984412702431113006858359767032
zaid-6969 <bawasamohammedzaid@gmail.com>
2026-03-12 15:16:13 +0530

    enable the cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
+const imageRouter = require("./router/image.router");
 
-const imageRouter = require("./router/image.router")
+app.use("/api/images", imageRouter);
 
-app.use("/api/images", imageRouter)
-
-module.exports = app
+module.exports = app;
